875e21bc96fe2bd3c4c703e281652f472f8dc865
Pam <prati.ma@hotmail.com>
2024-12-15 13:15:12 +0100

    Add css in the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import "./App.css";
+const items = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "Contact", href: "/contact" },
+];
 
 function App() {
   return (
     <>
-      <div>
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">Blog</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
+      <div className="header flex gap-20 items-center justify-center border-2">
+        <div className="logo p-0 m-0 leading-0 ">beauty App</div>
+        <ul className="flex gap-12 p-0 m-0 items-center justify-center">
+          {items.map((item) => {
+            return (
+              <li>
+                <a href="">{item.label}</a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
